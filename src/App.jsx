41e92004eb5b16170544c6f68d117d31ee9b860e
MyTution <rohitsonawane889@gmail.com>
2024-08-12 13:51:42 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,21 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const decrement = () => {
+    setCount(count > 0 ? count - 1 : 0);
+  };
+  const increment = () => {
+    setCount(count + 1);
+  };
   return (
     <>
       <h2>numbers:{count}</h2>
-      <button onClick={() =>setCount((count) => count + 1 )}> + </button> <br />
-      {/* <button onClick={() =>setCount((count) => count - 1 )}> - </button> */}
+
+      {/* <button onClick={() =>setCount((count) => count + 1 )}> + </button> <br />
+      <button onClick={() =>setCount((count) => count - 1 )}> - </button> */}
+
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
 
       <br />
 
