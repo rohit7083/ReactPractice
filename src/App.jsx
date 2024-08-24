a4d9a92c008d76551bc0ef26145d94909ebae735
MyTution <rohitsonawane889@gmail.com>
2024-08-24 17:46:47 +0530

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import BgColor from './compo/BgColor'
+import PassGen from './P3Password_gen/PassGen'
 function App() {
 
   return (
     <>
      <BrowserRouter>
      <Routes>
-      <Route path='/' element={<BgColor/>} />
+      {/* <Route path='/' element={<BgColor/>} /> */}
+      <Route path ={'/'} element={<PassGen/>}/>
      </Routes>
      
      </BrowserRouter>
--- src/compo/BgColor.jsx
+++ src/compo/BgColor.jsx
@@ -2,33 +2,29 @@ import React, { useState } from 'react'
 
 function BgColor() {
 
-    const[color,setColor]=useState("Green")
-   
-
-  return (
-    <>
-
-    <div className='w-full h-screen ' style={{backgroundColor:color}}>
+    const [color, setColor] = useState("black")
 
 
-<div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
-    <div className='flex flex-wrap justify-center gap-3  bg-white rounded-3xl px-3 py-2 shadow-xl'>
+    return (
+        <>
 
-        <button onClick={()=>{setColor("red")}}
-         className='outline-none px-4 py-1 rounded-full shadow-lg text-white ' style={{backgroundColor:"Red"}}>Red</button>
-        <button  onClick={()=>{setColor("Green")}}
-         className='outline-none px-4 py-1 rounded-full shadow-lg text-white ' style={{backgroundColor:"Green"}}>Green</button>
-        <button  onClick={()=>{setColor("Yellow")}}
-         className='outline-none px-4 py-1 rounded-full shadow-lg text-black ' style={{backgroundColor:"Yellow"}}>Yellow</button>
+            <div className='w-full h-screen ' style={{ backgroundColor: color }}>
+                <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+                    <div className='flex flex-wrap justify-center gap-3  bg-white rounded-3xl px-3 py-2 shadow-xl'>
 
-    </div>
+                        <button onClick={() => { setColor("red") }}
+                            className='outline-none px-4 py-1 rounded-full shadow-lg text-white ' style={{ backgroundColor: "Red" }}>Red</button>
+                        <button onClick={() => { setColor("Green") }}
+                            className='outline-none px-4 py-1 rounded-full shadow-lg text-white ' style={{ backgroundColor: "Green" }}>Green</button>
+                        <button onClick={() => { setColor("Yellow") }}
+                            className='outline-none px-4 py-1 rounded-full shadow-lg text-black ' style={{ backgroundColor: "Yellow" }}>Yellow</button>
 
-</div>
+                    </div>
+                </div>
+            </div>
 
-    </div>
-    
-    </>
-  )
+        </>
+    )
 }
 
 export default BgColor
