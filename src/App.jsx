891f47b16c3d6d2ee393a9b7453c4c2ddc3dc2a5
MyTution <rohitsonawane889@gmail.com>
2024-08-25 19:28:28 +0530

    React router Project

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,29 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import BgColor from './compo/BgColor'
 import PassGen from './P3Password_gen/PassGen'
 import CurrencyIndex from './P4CustomHook/CurrencyIndex'
+// import Header from './components/Header/Header'
+// import Footer from './components/Footer/Footer'
+import Home from './P5ReactRouter/Home'
 function App() {
 
   return (
     <>
      <BrowserRouter>
      <Routes>
+
       {/* <Route path='/' element={<BgColor/>} /> */}
       {/* <Route path ={'/'} element={<PassGen/>}/> */}
-<Route path='/' element={<CurrencyIndex/>}/>
+      {/* <Route path='/' element={<CurrencyIndex/>}/> */}
+
+
+<Route path='/' element={<Home/>}/>
+
+
+
+
 
      </Routes>
-     
      </BrowserRouter>
-
     </>
   )
 }
